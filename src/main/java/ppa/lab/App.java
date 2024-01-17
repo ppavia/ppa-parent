6563212ac885b5554f07d4470654aaaa4afa7ab8
PAVIA Pierre <pierre.pavia@assia.fr>
2024-01-17 18:30:57 +0100

    init commit

---
+++ src/main/java/ppa/lab/App.java
@@ -0,0 +1,13 @@
+package ppa.lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
